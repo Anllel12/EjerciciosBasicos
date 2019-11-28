9f6a66bb312389112553adc539afc9942fe83ac8
angel <angel@angel-pc>
2019-11-28 20:58:20 +0100

    Primer, segundo, tercer, cuarto y quinto ejercio hecho. Sexto y septimo ejercicio empezado.

---
+++ src/ejerciciosbasicos/Menos20.java
@@ -0,0 +1,21 @@
+
+package ejerciciosbasicos;
+
+/**
+ *
+ * @author Angel Esquinas
+ */
+public class Menos20 {
+    
+    public static void main(String[] args) {
+        Menos20 ejercicios=new Menos20();
+        System.out.println(ejercicios.menos20(18));
+        System.out.println(ejercicios.menos20(19));
+        System.out.println(ejercicios.menos20(20));
+        
+    }
+    
+    public boolean menos20(int a){
+       
+    } 
+}
